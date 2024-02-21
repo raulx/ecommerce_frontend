c5243696f23c2df2610aad622e04f4da78ef197c
Raulx103 <raulx103@gmail.com>
2024-02-21 21:51:55 +0530

    connected to backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
 
+// "https://rich-pear-coyote-wig.cyclic.app/"
+// "http://localhost:3300"
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [backend, setbackend] = useState("");
+  const handleClick = async () => {
+    try {
+      const res = await axios.get("https://rich-pear-coyote-wig.cyclic.app/");
+      setbackend(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>Hello world..</div>
+      <button onClick={handleClick}>connect backend.</button>
+      <h1>{backend}</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
