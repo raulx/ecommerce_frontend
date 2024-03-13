4d6202c39aa1bbba3ff54c82eb5c223bf24177b7
Raulx103 <raulx103@gmail.com>
2024-03-13 22:13:16 +0530

    fixed carousel responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import UseNavigationContext from "../hooks/useNavigationContext";
 
 function Navbar() {
   return (
-    <div className="bg-white flex gap-8 md:px-10 px-4 md:h-20 h-16 w-full shadow-sm items-center fixed z-10">
+    <div className="bg-white flex justify-center md:h-20 h-16 w-full shadow-sm items-center fixed z-10">
       <DesktopNav />
       <PhoneNav />
     </div>
@@ -25,78 +25,81 @@ function DesktopNav() {
     setMenuType("");
   };
   return (
-    <>
-      <div className="w-16 md:flex hidden">
-        <img
-          src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1709348782/ecommerce_assets/gn3jtvqnydprt1jymi3w.png"
-          className="w-full h-full"
-        />
+    <div className="h-full w-full flex items-center justify-around">
+      <div className="flex items-center gap-6 ml-4 h-full">
+        <div className="md:flex hidden w-14 h-12">
+          <img
+            src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710335387/ecommerce_assets/s6zpsidolphtf34czzuy.png"
+            className="w-full h-full"
+          />
+        </div>
+        <nav className="md:flex h-full items-center gap-4 hidden">
+          <li
+            className={`list-none  cursor-pointer font-bold  uppercase text-textmedium h-full text-center flex justify-center items-center border-b-4  ${
+              menuType === "mens" ? "border-b-red-600 " : "border-b-white"
+            } w-16`}
+            onMouseEnter={() => handleMouseEnter("mens")}
+            onMouseLeave={() => handleMouseLeave()}
+          >
+            Men
+          </li>
+          <li
+            className={`list-none cursor-pointer font-bold text-textmedium uppercase h-full text-center w-16 flex justify-center border-b-4 items-center ${
+              menuType === "women" ? "border-b-pink-600 " : "border-b-white"
+            }`}
+            onMouseEnter={() => handleMouseEnter("women")}
+            onMouseLeave={() => handleMouseLeave()}
+          >
+            Women
+          </li>
+          <li
+            className={`list-none cursor-pointer font-bold text-textmedium uppercase h-full justify-center items-center flex w-16 border-b-4  ${
+              menuType === "kids" ? "border-b-orange-500" : "border-b-white"
+            }`}
+            onMouseEnter={() => handleMouseEnter("kids")}
+            onMouseLeave={() => handleMouseLeave()}
+          >
+            Kids
+          </li>
+          <li
+            className={`list-none cursor-pointer font-bold  text-textmedium uppercase h-full justify-center items-center flex w-32  border-b-4  ${
+              menuType === "homeandliving"
+                ? "border-b-yellow-500"
+                : "border-b-white"
+            }`}
+            onMouseEnter={() => handleMouseEnter("homeandliving")}
+            onMouseLeave={() => handleMouseLeave()}
+          >
+            Home & Living
+          </li>
+          <li
+            className={`list-none cursor-pointer font-bold text-gray-700 uppercase h-full justify-center items-center flex w-16  border-b-4  ${
+              menuType === "beauty" ? "border-b-emerald-500" : "border-b-white"
+            }`}
+            onMouseEnter={() => handleMouseEnter("beauty")}
+            onMouseLeave={() => handleMouseLeave()}
+          >
+            Beauty
+          </li>
+          <li
+            className="list-none cursor-pointer font-bold text-gray-700 uppercase h-full justify-center items-center flex w-16 hover:border-b-4  hover:border-b-fuchsia-600 border-b-4 border-b-white"
+            onMouseEnter={() => handleMouseEnter("studio")}
+            onMouseLeave={() => handleMouseLeave()}
+          >
+            Studio
+          </li>
+        </nav>
       </div>
-      <nav className="md:flex h-full items-center gap-4 hidden">
-        <li
-          className={`list-none  cursor-pointer font-bold  uppercase text-textmedium h-full text-center flex justify-center items-center border-b-4  ${
-            menuType === "mens" ? "border-b-red-600 " : "border-b-white"
-          } w-16`}
-          onMouseEnter={() => handleMouseEnter("mens")}
-          onMouseLeave={() => handleMouseLeave()}
-        >
-          Men
-        </li>
-        <li
-          className={`list-none cursor-pointer font-bold text-textmedium uppercase h-full text-center w-16 flex justify-center border-b-4 items-center ${
-            menuType === "women" ? "border-b-pink-600 " : "border-b-white"
-          }`}
-          onMouseEnter={() => handleMouseEnter("women")}
-          onMouseLeave={() => handleMouseLeave()}
-        >
-          Women
-        </li>
-        <li
-          className={`list-none cursor-pointer font-bold text-textmedium uppercase h-full justify-center items-center flex w-16 border-b-4  ${
-            menuType === "kids" ? "border-b-orange-500" : "border-b-white"
-          }`}
-          onMouseEnter={() => handleMouseEnter("kids")}
-          onMouseLeave={() => handleMouseLeave()}
-        >
-          Kids
-        </li>
-        <li
-          className={`list-none cursor-pointer font-bold  text-textmedium uppercase h-full justify-center items-center flex w-32  border-b-4  ${
-            menuType === "homeandliving"
-              ? "border-b-yellow-500"
-              : "border-b-white"
-          }`}
-          onMouseEnter={() => handleMouseEnter("homeandliving")}
-          onMouseLeave={() => handleMouseLeave()}
-        >
-          Home & Living
-        </li>
-        <li
-          className={`list-none cursor-pointer font-bold text-gray-700 uppercase h-full justify-center items-center flex w-16  border-b-4  ${
-            menuType === "beauty" ? "border-b-emerald-500" : "border-b-white"
-          }`}
-          onMouseEnter={() => handleMouseEnter("beauty")}
-          onMouseLeave={() => handleMouseLeave()}
-        >
-          Beauty
-        </li>
-        <li
-          className="list-none cursor-pointer font-bold text-gray-700 uppercase h-full justify-center items-center flex w-16 hover:border-b-4  hover:border-b-fuchsia-600 border-b-4 border-b-white"
-          onMouseEnter={() => handleMouseEnter("studio")}
-          onMouseLeave={() => handleMouseLeave()}
-        >
-          Studio
-        </li>
-      </nav>
+
       <div
-        className={`flex-grow p-2 border rounded md:flex hidden items-center bg-neutralgray gap-4 group group-focus:bg-white ${
+        className={`p-2 border rounded md:flex hidden w-96  items-center bg-neutralgray gap-4 group group-focus:bg-white ${
           isSearchFocussed && "bg-white"
         }`}
       >
         <CiSearch className="text-xl font-bold" />
         <input
           type="text"
-          className="flex-grow group outline-none focus:bg-white bg-neutralgray"
+          className="group grow outline-none focus:bg-white bg-neutralgray"
           placeholder="Search for products,brands and more"
           onFocus={() => setIsSearchFocussed(true)}
           onBlur={() => setIsSearchFocussed(false)}
@@ -124,7 +127,7 @@ function DesktopNav() {
           <span className="font-bold text-sm text-textmedium">Bag</span>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
--- src/pages/LandingPageScreens/LandingPageDesktop.jsx
+++ src/pages/LandingPageScreens/LandingPageDesktop.jsx
@@ -29,38 +29,87 @@ function LandingPageDesktop() {
           Medal worthy brands to bag
         </h1>
       </div>
-      <Carousel slides={4} autoslide={true} autoSlideInterval={7000}>
+      <Carousel slides={4} autoslide={false} autoSlideInterval={7000}>
         <div className="flex w-full h-full">
-          <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710211220/ecommerce_assets/discounts/fg7ajh5qewjyrczculga.jpg" />
-          <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710176488/ecommerce_assets/discounts/ozuxhjw4r4ulk6qwoazh.jpg" />
-          <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710176495/ecommerce_assets/discounts/bgp3ymeay4a2a7aug8tk.jpg" />
-          <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710176527/ecommerce_assets/discounts/fyjjojtiz9askieyju2q.jpg" />
-          <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710176488/ecommerce_assets/discounts/xolac0okeboky0rkrbcb.jpg" />
-          <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710176488/ecommerce_assets/discounts/teqmk3oawiq2ptkh9yfs.jpg" />
+          <div className="w-80">
+            <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710211220/ecommerce_assets/discounts/fg7ajh5qewjyrczculga.jpg" />
+          </div>
+          <div className="w-80">
+            <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710176488/ecommerce_assets/discounts/ozuxhjw4r4ulk6qwoazh.jpg" />
+          </div>
+          <div className="w-80">
+            <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710176495/ecommerce_assets/discounts/bgp3ymeay4a2a7aug8tk.jpg" />
+          </div>
+          <div className="w-80">
+            <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710176527/ecommerce_assets/discounts/fyjjojtiz9askieyju2q.jpg" />
+          </div>
+
+          <div className="w-80">
+            <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710176488/ecommerce_assets/discounts/xolac0okeboky0rkrbcb.jpg" />
+          </div>
+          <div className="w-80">
+            <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710176488/ecommerce_assets/discounts/teqmk3oawiq2ptkh9yfs.jpg" />
+          </div>
         </div>
         <div className="flex">
-          <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710176569/ecommerce_assets/discounts/d09gxayvmmjwzm84kp2w.jpg" />
-          <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710176568/ecommerce_assets/discounts/nximgztg0qcqwnb31o6q.jpg" />
-          <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710176496/ecommerce_assets/discounts/qlxykb0du1yp6uqktoa9.jpg" />
-          <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710176569/ecommerce_assets/discounts/v2piwswqot49lurveutq.jpg" />
-          <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710176488/ecommerce_assets/discounts/aoh9lm777srbavhr0psb.jpg" />
-          <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710176488/ecommerce_assets/discounts/l1vstiu8sla5tptdd0wg.jpg" />
+          <div className="w-80">
+            <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710176569/ecommerce_assets/discounts/d09gxayvmmjwzm84kp2w.jpg" />
+          </div>
+          <div className="w-80">
+            <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710176568/ecommerce_assets/discounts/nximgztg0qcqwnb31o6q.jpg" />
+          </div>
+          <div className="w-80">
+            <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710176496/ecommerce_assets/discounts/qlxykb0du1yp6uqktoa9.jpg" />
+          </div>
+          <div className="w-80">
+            <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710176569/ecommerce_assets/discounts/v2piwswqot49lurveutq.jpg" />
+          </div>
+          <div className="w-80">
+            <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710176488/ecommerce_assets/discounts/aoh9lm777srbavhr0psb.jpg" />
+          </div>
+          <div className="w-80">
+            <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710176488/ecommerce_assets/discounts/l1vstiu8sla5tptdd0wg.jpg" />
+          </div>
         </div>
         <div className="flex">
-          <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710176496/ecommerce_assets/discounts/xeitqgis3gmc5hfgcfyt.jpg" />
-          <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710177209/ecommerce_assets/discounts/oomivjljueuujyewvmgw.jpg" />
-          <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710176488/ecommerce_assets/discounts/ryvkd59eijnn9uros0kt.jpg" />
-          <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710212060/ecommerce_assets/discounts/kgwa7m8pwfryvhrguvgj.jpg" />
-          <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710212383/ecommerce_assets/discounts/yuk0x4j7ltotslmxgkbm.jpg" />
-          <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710176568/ecommerce_assets/discounts/xooeguedgfjzv9vmshtt.jpg" />
+          <div className="w-80">
+            <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710176496/ecommerce_assets/discounts/xeitqgis3gmc5hfgcfyt.jpg" />
+          </div>
+          <div className="w-80">
+            <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710177209/ecommerce_assets/discounts/oomivjljueuujyewvmgw.jpg" />
+          </div>
+          <div className="w-80">
+            <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710176488/ecommerce_assets/discounts/ryvkd59eijnn9uros0kt.jpg" />
+          </div>
+          <div className="w-80">
+            <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710212060/ecommerce_assets/discounts/kgwa7m8pwfryvhrguvgj.jpg" />
+          </div>
+          <div className="w-80">
+            <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710212383/ecommerce_assets/discounts/yuk0x4j7ltotslmxgkbm.jpg" />
+          </div>
+          <div className="w-80">
+            <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710176568/ecommerce_assets/discounts/xooeguedgfjzv9vmshtt.jpg" />
+          </div>
         </div>
         <div className="flex">
-          <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710176566/ecommerce_assets/discounts/regn0jxrxfurs7uykujz.jpg" />
-          <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710176496/ecommerce_assets/discounts/t1kydokmjqbycqoljg3m.jpg" />
-          <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710212691/ecommerce_assets/discounts/ad2oq2gi7p9cdglsmzq7.jpg" />
-          <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710212859/ecommerce_assets/discounts/awvtzql2tfcwqtnzuawj.jpg" />
-          <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710212966/ecommerce_assets/discounts/djzgmoegdrhi9cjnubih.jpg" />
-          <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710176567/ecommerce_assets/discounts/jzfhqcmafpmewfk28kyy.jpg" />
+          <div className="w-80">
+            <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710176566/ecommerce_assets/discounts/regn0jxrxfurs7uykujz.jpg" />
+          </div>
+          <div className="w-80">
+            <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710176496/ecommerce_assets/discounts/t1kydokmjqbycqoljg3m.jpg" />
+          </div>
+          <div className="w-80">
+            <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710212691/ecommerce_assets/discounts/ad2oq2gi7p9cdglsmzq7.jpg" />
+          </div>
+          <div className="w-80">
+            <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710212859/ecommerce_assets/discounts/awvtzql2tfcwqtnzuawj.jpg" />
+          </div>
+          <div className="w-80">
+            <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710212966/ecommerce_assets/discounts/djzgmoegdrhi9cjnubih.jpg" />
+          </div>
+          <div className="w-80">
+            <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710176567/ecommerce_assets/discounts/jzfhqcmafpmewfk28kyy.jpg" />
+          </div>
         </div>
       </Carousel>
       <div className="text-4xl uppercase font-bold my-8">
@@ -70,20 +119,44 @@ function LandingPageDesktop() {
       </div>
       <Carousel slides={2} autoslide={true}>
         <div className="flex h-full">
-          <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710217124/ecommerce_assets/discounts/globalbrands/bgsxlv9mslxvm2r3nnhf.jpg" />
-          <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710217124/ecommerce_assets/discounts/globalbrands/xqrtg54ahaviztr6cxi8.jpg" />
-          <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710217124/ecommerce_assets/discounts/globalbrands/r27v0scfdhcblimljvxa.jpg" />
-          <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710217124/ecommerce_assets/discounts/globalbrands/n81g416exwxce3dhw9fu.jpg" />
-          <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710217123/ecommerce_assets/discounts/globalbrands/ahri7s6apvseoonxgn50.jpg" />
-          <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710217123/ecommerce_assets/discounts/globalbrands/rvnnw907x4e76oxhyl5g.jpg" />
+          <div className="w-80">
+            <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710217124/ecommerce_assets/discounts/globalbrands/bgsxlv9mslxvm2r3nnhf.jpg" />
+          </div>
+          <div className="w-80">
+            <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710217124/ecommerce_assets/discounts/globalbrands/xqrtg54ahaviztr6cxi8.jpg" />
+          </div>
+          <div className="w-80">
+            <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710217124/ecommerce_assets/discounts/globalbrands/r27v0scfdhcblimljvxa.jpg" />
+          </div>
+          <div className="w-80">
+            <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710217124/ecommerce_assets/discounts/globalbrands/n81g416exwxce3dhw9fu.jpg" />
+          </div>
+          <div className="w-80">
+            <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710217123/ecommerce_assets/discounts/globalbrands/ahri7s6apvseoonxgn50.jpg" />
+          </div>
+          <div className="w-80">
+            <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710217123/ecommerce_assets/discounts/globalbrands/rvnnw907x4e76oxhyl5g.jpg" />
+          </div>
         </div>
         <div className="flex h-full">
-          <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710218344/ecommerce_assets/discounts/globalbrands/ncf08ruozmzdzyejz1vb.jpg" />
-          <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710218596/ecommerce_assets/discounts/globalbrands/fift9mhxnbuh638edjye.jpg" />
-          <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710218596/ecommerce_assets/discounts/globalbrands/xslsifcbkl1gmv7slgig.jpg" />
-          <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710218596/ecommerce_assets/discounts/globalbrands/rkltlisrdqwgvgbndyqw.jpg" />
-          <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710218345/ecommerce_assets/discounts/globalbrands/axtsexj3mbjruwhvewno.jpg" />
-          <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710218345/ecommerce_assets/discounts/globalbrands/owffdgi3v53oiodpoiol.jpg" />
+          <div className="w-80">
+            <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710218344/ecommerce_assets/discounts/globalbrands/ncf08ruozmzdzyejz1vb.jpg" />
+          </div>
+          <div className="w-80">
+            <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710218596/ecommerce_assets/discounts/globalbrands/fift9mhxnbuh638edjye.jpg" />
+          </div>
+          <div className="w-80">
+            <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710218596/ecommerce_assets/discounts/globalbrands/xslsifcbkl1gmv7slgig.jpg" />
+          </div>
+          <div className="w-80">
+            <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710218596/ecommerce_assets/discounts/globalbrands/rkltlisrdqwgvgbndyqw.jpg" />
+          </div>
+          <div className="w-80">
+            <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710218345/ecommerce_assets/discounts/globalbrands/axtsexj3mbjruwhvewno.jpg" />
+          </div>
+          <div className="w-80">
+            <img src="https://res.cloudinary.com/dj5yf27lr/image/upload/v1710218345/ecommerce_assets/discounts/globalbrands/owffdgi3v53oiodpoiol.jpg" />
+          </div>
         </div>
       </Carousel>
       <div className="text-4xl uppercase font-bold my-8">
